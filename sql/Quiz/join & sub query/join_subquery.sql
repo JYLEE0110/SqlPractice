6becf940dbe920510337d6e61aa6b0072b75cc69
JY LEE <wndyd0110@naver.com>
2023-04-15 00:50:41 +0900

    Update join_subquery.sql

--- sql/Quiz/join & sub query/join_subquery.sql
+++ sql/Quiz/join & sub query/join_subquery.sql
@@ -29,10 +29,7 @@ where name not in(select distinct c.name
                 where c.custid = o.custid);
                 
 --(9) 주문금액의총액과주문의평균금액
-select avg(saleprice)
-from orders;
-
-select sum(saleprice) 
+select sum(saleprice), avg(saleprice)
 from orders;
 
 -- (10) 고객의이름과고객별구매액
@@ -49,21 +46,37 @@ order by c.name, b.bookname;
 
 --(12) 도서의가격(Book 테이블)과판매가격(Orders 테이블)의차이가가장많은주문
 select *
-from book b, orders o
-where b.bookid = o.bookid
-and (b.price - o.saleprice) = (select max(b.price - o.saleprice)
-                                from book b, orders o
-                                where b.bookid = o.bookid);
-                                
---(13) 도서의판매액평균보다
--- 자신의구매액평균이더높은고객의이름
-select avg(saleprice)
-from orders;
+from orders join book using(bookid)
+where (price -saleprice) = (select max(price - saleprice)
+                                from orders join book using(bookid));
 
-select c.name, round(avg(saleprice))
+--(13) 도서의판매액평균보다자신의구매액평균이더높은고객의이름
+select c.name, avg(o.saleprice)
 from customer c, orders o
 where c.custid = o.custid
 group by c.name
-having avg(o.saleprice) < (select avg(saleprice)
-from orders);
+having avg(o.saleprice) > (select avg(saleprice)from orders);
+
+--3. 마당서점에서 다음의 심화된 질문에 대해 SQL 문을 작성하시오.
+--(1) 박지성이 구매한 도서의 출판사와
+-- 같은 출판사에서 도서를 구매한 고객의 이름
+select c.name, b.publisher
+from customer c, orders o, book b
+where c.custid = o.custid and o.bookid = b.bookid
+and b.publisher in (select b.publisher
+                    from customer c, orders o, book b
+                    where c.custid = o.custid and o.bookid = b.bookid
+                    and c.name = '박지성') and c.name != '박지성' ;                    
+
+
+-- (2) 두 개 이상의 서로 다른 출판사에서 
+--도서를 구매한 고객의 이름
+select c.name, count(distinct publisher)
+from customer c, orders o, book b
+where c.custid = o.custid and o.bookid = b.bookid
+group by c.name
+having count(distinct publisher) >=2;
+
+
+
 
